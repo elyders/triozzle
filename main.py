2bf83316321fc6b0bb950773d626a421d3f680d1
elyders <elyders@yahoo.com>
2023-08-15 09:50:27 -0600

    Update main.py
    removed pyperclip

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import time
 from datetime import datetime, timedelta
 import asyncio
 from pyodide.ffi import to_js
-import pyperclip
 
 # global variables
 guess_list = [0 for i in range(3)]
@@ -499,11 +498,6 @@ def handle_msg(msg_type, msg_text):
         # Store these in a debug file once app in production ready
         x = 1
         #print(msg_text)
-
-#################################################
-def copyResults():
-    copyText = document.getElementById("endScore").innerHTML
-    pyperclip.copy(copyText)
 
 #################################################
 
