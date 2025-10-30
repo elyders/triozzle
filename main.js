c28d3b5a287bd3bd7519707823e0db66e3df4030
elyders <elyders@yahoo.com>
2025-10-29 20:22:55 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './shadcn.css';
-import App from './main/App';
+import App from "https://raw.githubusercontent.com/elyders/triozzle/refs/heads/main/App.js";
 
 const root = createRoot(document.getElementById('app'));
 root.render(React.createElement(App, null));
