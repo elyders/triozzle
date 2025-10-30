66e415ec97be7214d5e06439b4c67426cb45043c
elyders <elyders@yahoo.com>
2025-10-29 19:54:14 -0600

    Create App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './Home.jsx';
+
+export default function App() {
+  return React.createElement(
+    HashRouter,
+    null,
+    React.createElement(
+      Routes,
+      null,
+      React.createElement(Route, { path: "/", element: React.createElement(HomePage, null) })
+    )
+  );
+}
